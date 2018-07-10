e2a7c0d016b37c17de2d9d2ceb48c8dadfb25c75
Ben Clark <benclarkphoto@mac.com>
2018-07-09 19:24:12 -0700

    https://github.com/BenShigeruClark/serverTest01

--- gitServer.js
+++ gitServer.js
@@ -4,6 +4,8 @@ var PORT = 7000;
 
 var secondPORT = 7500;
 
+
+
 function handleRequest(request, response) {
     response.end("You are a Great Listener!" + request.url);
 }
@@ -19,9 +21,9 @@ function secondRequest(request, response) {
     response.end("You are a Horrible Listener!" + request.url);
 }
 
-var server = http.createServer(secondRequest);
+var serverTwo = http.createServer(secondRequest);
 
-server.listen(secondPORT, function() {
+serverTwo.listen(secondPORT, function() {
 
     console.log("Server listening on: http://localhost:" + secondPORT);
 });
